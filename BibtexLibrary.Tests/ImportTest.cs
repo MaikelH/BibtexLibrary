aa1d90d6f0b07146e4331e8ee0bfd3c24467aaa1
M. Hofman <m.hofman@chronos-technology.nl>
2015-07-26 12:23:02 +0200

    Added semicolon as valid text and implemented tests for the full import.

--- BibtexLibrary.Tests/ImportTest.cs
+++ BibtexLibrary.Tests/ImportTest.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
-using BibtexLibrary;
+﻿using System.Collections.Generic;
+using System.Linq;
+using BibtexLibrary.Parser.Nodes;
 using NUnit.Framework;
+using BibtexFile = BibtexLibrary.BibtexFile;
 
 namespace BibtexImporter.Tests
 {
@@ -20,8 +22,16 @@ namespace BibtexImporter.Tests
                                                                                   topic = {multivariate-statistics;market-research;}
                                                                                  }");
 
-            Assert.AreEqual(1, file.Entries);
+            Assert.AreEqual(1, file.Entries.Count);
             Assert.AreEqual(file.Entries.ToList()[0].Key, "aaker:1981a");
+            Assert.AreEqual(file.Entries.ToList()[0].Type, "book");
+
+            Dictionary<string, string> tags = file.Entries.ToList()[0].Tags;
+            Assert.AreEqual("David A. Aaker", tags["author"]);
+            Assert.AreEqual("The Scientific Press", tags["publisher"]);
+            Assert.AreEqual("1981", tags["year"]);
+            Assert.AreEqual("multivariate-statistics;market-research;", tags["topic"]);
+            Assert.AreEqual("Multivariate Analysis in Marketing", tags["title"]);
         }
     }
 }
